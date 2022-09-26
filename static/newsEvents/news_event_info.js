e228986c8e6573d08333a7ad1d11dd748d8af970
jonathanw <jonwhewaycode@outlook.com>
2022-09-26 18:04:51 +0100

    push to pages

--- static/newsEvents/news_event_info.js
+++ static/newsEvents/news_event_info.js
@@ -1,5 +1,9 @@
 "use strict";
 
+/*
+     To add news or events lay each stroy out in an array format put the story within quotes and add a comma "<story>", 
+*/
+
 let news =[
     // "change this info and it will appear on the landing page under announcements",
 ]
@@ -11,5 +15,5 @@ let events =[
 let events_dates_times = [
      "Monday 12th September 5-8pm",
      "Wednesday 14th September",
-     "Friday 21st October 5-9pm",
+     "Friday 21st October 5-10pm",
 ]
